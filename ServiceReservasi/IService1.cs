0efa3137fcefd5247bb59656c4d671ed63225f62
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-12-13 11:04:28 +0700

    add method login, register, update & delete

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -23,6 +23,30 @@ namespace ServiceReservasi
         List<DetailLokasi> DetailLokasi();//nampilin detail lokasi
         [OperationContract]
         List<Pemesanan> Pemesanan();
+
+        [OperationContract]
+        string Login(string username, string password);
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+        [OperationContract]
+        string DeleteRegister(string username);
+        [OperationContract]
+        List<DataRegister> DataRegist();
+    }
+
+    [DataContract]
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
     }
 
     [DataContract]
